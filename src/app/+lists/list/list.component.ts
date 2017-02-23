6e537f6beebc95274df94d8a6a63c2c250be8be0
Blackbaud Sky Build User <sky-build-user@blackbaud.com>
2017-02-22 23:47:58 -0500

    Updated styles

--- src/app/+lists/list/list.component.ts
+++ src/app/+lists/list/list.component.ts
@@ -1,12 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { ListService } from '../../shared/services';
+import { List } from '../../shared/list';
 
 @Component({
   selector: 'app-list',
   template: `
-    <h1>List</h1>
+    <h1>{{list?.name}}</h1>
   `
 })
-export class ListComponent implements OnInit {
-  constructor() { }
-  ngOnInit() {}
+export class ListComponent implements OnInit, OnDestroy {
+  list: List;
+  private sub: any;
+
+  constructor(
+    private listService: ListService,
+    private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.sub = this.route.params.subscribe(params => {
+       const id = Number(params['id']);
+       this.listService.getById(id).then(data => this.list = data);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 }

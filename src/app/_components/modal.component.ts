082c1df2566b428a62e71d9c9cad93f258bfdd66
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2017-08-09 16:10:54 -0400

    Cleaned up modal

--- src/app/_components/modal.component.ts
+++ src/app/_components/modal.component.ts
@@ -8,21 +8,4 @@ import { Component, Input } from '@angular/core';
 export class ModalComponent {
   @Input()
   public isVisible = false;
-
-  public constructor() {
-  }
-
-  // public ngAfterContentInit(): void {
-  //   console.log(this.isVisibleStream);
-  //   this.isVisibleStream.subscribe((isVisible: boolean) => {
-  //     console.log('isVisible?', isVisible);
-  //     this.isVisible = isVisible;
-  //   });
-  // }
-
-  // public ngOnChanges(changes: any): void {
-  //   if (changes.isVisibleStream && !changes.isVisibleStream.firstChange) {
-  //     console.log('changes?', changes);
-  //   }
-  // }
 }

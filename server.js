ed449a03eaad7e26ce9af202b3ccc53e17f0eabe
Steve Brush <steve.brush@blackbaud.com>
2018-04-01 17:20:44 -0400

    Procfile (#19)

--- server.js
+++ server.js
@@ -10,8 +10,9 @@ app.use(express.static(__dirname + '/dist'));
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.get('*', function (req, res) {
+  const index = path.join(__dirname, 'dist', 'index.html');
+  res.sendFile(index);
 });
 
 const port = process.env.PORT || 8080;

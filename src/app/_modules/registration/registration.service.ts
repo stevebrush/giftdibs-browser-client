4f6660c8b0bb756782cdec8a6d085fd1c5ba7556
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-04-01 23:04:32 -0400

    Implemented diff API URL for env

--- src/app/_modules/registration/registration.service.ts
+++ src/app/_modules/registration/registration.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { environment } from '../../../environments/environment';
+
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class RegistrationService {
-  private resourceUrl = 'http://localhost:8080/v1/auth';
+  private resourceUrl = environment.apiUrl + '/auth';
 
   constructor(
     private http: HttpClient

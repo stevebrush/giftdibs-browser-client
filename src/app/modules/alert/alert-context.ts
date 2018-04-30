7ba9fd4c6d3690fb897b0a84f4e1b685bf95863a
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-04-29 21:26:34 -0400

    Fixed aot errors

--- src/app/modules/alert/alert-context.ts
+++ src/app/modules/alert/alert-context.ts
@@ -1,10 +1,5 @@
-import {
-  Injectable
-} from '@angular/core';
-
 import { Alert } from './alert';
 
-@Injectable()
 export class AlertContext {
   constructor(
     public alert: Alert
--- src/app/modules/confirm/confirm-context.ts
+++ src/app/modules/confirm/confirm-context.ts
@@ -1,13 +1,8 @@
-import {
-  Injectable
-} from '@angular/core';
-
 import { Subject } from 'rxjs/Subject';
 
 import { ConfirmAnswer } from './confirm-answer';
 import { ConfirmConfig } from './confirm-config';
 
-@Injectable()
 export class ConfirmContext {
   public answerStream = new Subject<ConfirmAnswer>();
 

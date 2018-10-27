556559358842946b3e9c041a88acc06d8dbf5015
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-10-27 11:56:41 -0400

    Updated API URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://giftdibs-api.firebaseapp.com/v1',
+  apiUrl: 'https://api.giftdibs.com/v1',
   scraperUrl: 'https://giftdibs-url-scraper.firebaseapp.com/v1'
 };

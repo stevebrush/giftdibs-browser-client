bfcf6aeeda0bdbc606ba6b73b806a6a24bd48760
sajeetharan <sajeetharan@outlook.com>
2018-10-30 23:22:16 +0530

    Add jewel module and component

---
+++ src/app/features/jewel/jewel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gd-jewel',
+  templateUrl: './jewel.component.html',
+  styleUrls: ['./jewel.component.scss']
+})
+export class JewelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/features/jewel/jewel.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { JewelComponent } from './jewel.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [JewelComponent]
+})
+export class JewelModule { }

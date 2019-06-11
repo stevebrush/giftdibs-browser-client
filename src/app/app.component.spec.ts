f5b57d7ddc055bcfc8583a76dd0aa1e3e4300d9b
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2019-06-11 12:38:04 -0400

    Updated packages

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,11 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('foobar', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AppComponent]
+    });
+  });
+  it('', () => {});
+});
